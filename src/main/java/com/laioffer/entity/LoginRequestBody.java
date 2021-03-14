c38d08eac2423646562f1d6d8896a45ecd70280d
Siqi Li <siqili1226@gmail.com>
2021-03-14 12:22:24 +0800

    update Controller

---
+++ src/main/java/com/laioffer/entity/LoginRequestBody.java
@@ -0,0 +1,13 @@
+package com.laioffer.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class LoginRequestBody {
+
+    @JsonProperty("user_id")
+    public String userId;
+
+    public String password;
+}
+
+
