751336313bb46e2280d3fc8c79073ebfdc5f32dd
siqi-ontheway <siqili1226@gmail.com>
2021-03-14 17:07:27 -0700

    create db

--- src/main/java/com/laioffer/db/MySQLDBUtil.java
+++ src/main/java/com/laioffer/db/MySQLDBUtil.java
@@ -3,7 +3,7 @@ package com.laioffer.db;
 public class MySQLDBUtil {
     private static final String INSTANCE = "database-2.cq7pevcaqc2i.us-east-2.rds.amazonaws.com";
     private static final String PORT_NUM = "3306";
-    public static final String DB_NAME = "ecommerce";
+    public static final String DB_NAME = "job";
     private static final String USERNAME = "admin";
     private static final String PASSWORD = "Lsq941226!!";
     public static final String URL = "jdbc:mysql://"
